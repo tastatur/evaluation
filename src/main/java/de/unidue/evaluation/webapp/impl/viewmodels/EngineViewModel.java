f328614128f6309cf983f24dffad6df4f91cead7
tastatur <repkov.denis@gmail.com>
2015-08-09 19:13:40 +0200

    Evaluierung, Engine.zul

--- src/main/java/de/unidue/evaluation/webapp/impl/viewmodels/EngineViewModel.java
+++ src/main/java/de/unidue/evaluation/webapp/impl/viewmodels/EngineViewModel.java
@@ -1,8 +1,11 @@
 package de.unidue.evaluation.webapp.impl.viewmodels;
 
 import de.unidue.evaluation.webapp.AvailableQueriesService;
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Radiogroup;
 
 import java.io.Serializable;
 import java.util.List;
@@ -10,10 +13,23 @@ import java.util.List;
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class EngineViewModel implements Serializable {
 
+    private Integer qualityOfEngine = 1;
+    private Integer speedOfEngine = 1;
+
     @WireVariable
     private AvailableQueriesService availableQueriesService;
 
     public List<String> getSearchQueries() {
         return availableQueriesService.getAllAvailableSearchQueries();
+    }
+
+    @Command
+    public void rateQuality(@BindingParam("checked") Radiogroup qualityRadioGroup) {
+        qualityOfEngine = Integer.valueOf(qualityRadioGroup.getSelectedItem().getLabel());
+    }
+
+    @Command
+    public void rateSpeed(@BindingParam("checked") Radiogroup speedRadioGroup) {
+        speedOfEngine = Integer.valueOf(speedRadioGroup.getSelectedItem().getLabel());
     }
 }
